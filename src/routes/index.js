a3fe811196e32544735a3fe70952f483927d4abf
Gabriel Stuani <bielstuani@gmail.comm>
2024-02-27 21:23:24 -0300

    Atualizando a parte dde registro

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/login', async (req, res) => {
+    res.render('login' , { messages: { error: 'Sua mensagem de erro aqui' } });
+});
+
+router.get('/registro', function(req, res) {
+  res.render('registro', { messages: { error: 'Sua mensagem de erro aqui' } });
+});
+
+
+module.exports = router;
